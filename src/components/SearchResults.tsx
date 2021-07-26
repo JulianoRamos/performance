7208e57c3c3c40d90fa92452b0adcab0416cee2c
Juliano Ramos <490meister@gmail.com>
2021-07-26 13:57:03 -0300

    feat(virtualized): adicionado a virtualização

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { List, ListRowRenderer } from 'react-virtualized';
+
 import ProductItem from './ProductItem';
 
 interface ISearchResultsProps {
@@ -14,15 +17,26 @@ interface ISearchResultsProps {
 
 const SearchResults: React.FC<ISearchResultsProps> = ({ results, totalPrice, onAddToWishlist }: ISearchResultsProps) => {
  
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem product={results[index]} onAddToWishlist={onAddToWishlist} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
-        return (
-          <ProductItem key={product.id} product={product} onAddToWishlist={onAddToWishlist} />
-        );
-      })}
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
